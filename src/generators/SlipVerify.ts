ee761b1f9eb2d5329763d43dd5439c2433a6adfd
Maythiwat Chomchuen <maythiwat.chom@gmail.com>
2022-12-18 16:57:17 +0700

    chore: describe about slip verify

--- src/generators/SlipVerify.ts
+++ src/generators/SlipVerify.ts
@@ -1,5 +1,14 @@
 import { encode, tag, withCrcTag } from '../lib/TagUtils'
 
+/**
+ * Generate Slip Verify QR Code
+ * 
+ * This also called "Mini-QR" that embedded in slip used for verify transactions
+ *
+ * @param sendingBankId - Bank Code
+ * @param transactionRefId - Transaction Reference
+ * @returns QR Instance with TLV Tags
+ */
 export function generate(sendingBankId: string, transactionRefId: string) {
   const payload = [
     tag('00', encode([
