88c7beacdd481b1a197514da8d4b0589b00eeaf5
Maythiwat Chomchuen <maythiwat.chom@gmail.com>
2025-12-03 15:10:04 +0700

    fix: slip verify auto fix bad crc

--- src/validate/SlipVerify.ts
+++ src/validate/SlipVerify.ts
@@ -4,9 +4,20 @@ import { parse } from '@/lib/parser'
  * Validate & extract data from Slip Verify QR (for use with Bank Open API)
  *
  * @param payload - QR Code Payload
+ * @param crcAutoFix - Attempt to fix bad checksum from bank app
  * @returns Bank code and Transaction reference or null if payload invalid
  */
-export function slipVerify(payload: string) {
+export function slipVerify(payload: string, crcAutoFix = true) {
+  if (crcAutoFix) {
+    const idx = payload.lastIndexOf('9104')
+    if (idx != -1) {
+      const crc = payload.substring(idx + 4)
+      if (crc.length > 0 && crc.length < 4) {
+        payload = payload.substring(0, idx + 4) + crc.padStart(4, '0')
+      }
+    }
+  }
+
   const ppqr = parse(payload, true)
 
   const apiType = ppqr?.getTagValue('00', '00')
